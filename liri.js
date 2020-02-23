673da71740bd8f257aff7b45df97ac81959bfb1c
Brandon Williams <brando5393@gmail.com>
2020-02-22 19:49:22 -0600

    added code for app functions. Code broken as of yet in multiple places look further into looping through array for movie ratings and song request syntax

--- liri.js
+++ liri.js
@@ -3,7 +3,10 @@ var keys = require("./keys.js");
 var axios = require("axios");
 var moment = require("moment");
 var spotifyAPI = require("node-spotify-api");
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+});
 
 
 var action = process.argv[4];
@@ -13,16 +16,16 @@ var request = process.argv[5];
 
 // application functions
 
-function findConcert(){
+function findConcert(request) {
     // use axios to search for requested concert using bands in town api
     // data should allow for name of venue, venue location and pretty date of event
 
-    axios.get("https://rest.bandsintown.com/artists/" + request + "/events?app_id=codingbootcamp").then(function(response){
+    axios.get("https://rest.bandsintown.com/artists/" + request + "/events?app_id=codingbootcamp").then(function (response) {
         // if concert data is found print the data the screen
         console.log("===CONCERT INFORMATION===\n");
         console.log(response);
         console.log("VENUE: " + response.venue.name + "\nLOCATION: " + response.venue.city + ", " + response.venue.country + "\nDATE: " + moment(response.datetime).format('MMM Do YYYY'));
-    }).catch(function(error){
+    }).catch(function (error) {
         if (error.response) {
             // The request was made and the server responded with a status code
             // that falls out of the range of 2xx
@@ -32,31 +35,100 @@ function findConcert(){
             console.log(error.response.status);
             console.log("=====HEADERS STATUS=====")
             console.log(error.response.headers);
-          } else if (error.request) {
+        } else if (error.request) {
             // The request was made but no response was received
             // `error.request` is an object that comes back with details pertaining to the error that occurred.
             console.log(error.request);
-          } else {
+        } else {
             // Something happened in setting up the request that triggered an Error
             console.log("Error", error.message);
-          }
-          console.log(error.config);
+        }
+        console.log(error.config);
+    });
+}
+
+
+
+
+// function findMovie(request) {
+//     // use axios to search for requested concert using bands in town api
+//     // data should allow for name of venue, venue location and pretty date of event
+
+//     axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + request).then(function (response) {
+//         // if concert data is found print the data the screen
+//         console.log("===MOVIE INFORMATION===\n");
+//         console.log(response);
+//         console.log("TITLE: " + response.Title + "\nRELEASE YEAR: " + response.Year + "\nIMDB RATING: " + movie rating here + "\nROTTEN TOMATOES RATING: " + movie rating here + "\nCOUNTRY: " + response.Country + "\nLANGUAGE: " + response.Language + "\nPLOT: " + response.Plot + "\nACTORS: " + response.Actors);
+//     }).catch(function (error) {
+//         if (error.response) {
+//             // The request was made and the server responded with a status code
+//             // that falls out of the range of 2xx
+//             console.log("=====DATA=====");
+//             console.log(error.response.data);
+//             console.log("=====STATUS=====");
+//             console.log(error.response.status);
+//             console.log("=====HEADERS STATUS=====")
+//             console.log(error.response.headers);
+//         } else if (error.request) {
+//             // The request was made but no response was received
+//             // `error.request` is an object that comes back with details pertaining to the error that occurred.
+//             console.log(error.request);
+//         } else {
+//             // Something happened in setting up the request that triggered an Error
+//             console.log("Error", error.message);
+//         }
+//         console.log(error.config);
+//     });
+// }
+
+function findSong(request) {
+    spotify.search({
+        type: 'track',
+        query: request
+    }, function (error, data) {
+        if (request === "") {
+            request = "The Sign";
+            console.log("===SONG INFORMATION===\n");
+            console.log(data);
+        } else {
+            console.log(data);
+
+        }
+    }).catch(function (error) {
+        if (error.response) {
+            //         // The request was made and the server responded with a status code
+            //         // that falls out of the range of 2xx
+            console.log("=====DATA=====");
+            console.log(error.response.data);
+            console.log("=====STATUS=====");
+            console.log(error.response.status);
+            console.log("=====HEADERS STATUS=====")
+            console.log(error.response.headers);
+        } else if (error.request) {
+            //         // The request was made but no response was received
+            //         // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+        } else {
+            //         // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+        }
+        console.log(error.config);
     });
 }
 
 console.log(action);
-console.log(typeof(action));
+console.log(typeof (action));
 
-switch(action){
+switch (action) {
     case "concert-this":
-        findConcert();
-        
+        findConcert(request);
+
         break;
     case "spotify-this-song":
-        //spotify code here
+        findSong(request)
         break;
     case "movie-this":
-        // movie code here
+        findMovie(request)
         break;
     case "do-what-it-says":
         // do code here
